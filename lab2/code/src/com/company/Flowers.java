548608cd87070eab7882e3ffb3ee8ae4d48c5262
dumi <lenca_1995@mail.ru>
2018-03-13 08:42:13 +0200

    c

--- lab2/code/src/com/company/Flowers.java
+++ lab2/code/src/com/company/Flowers.java
@@ -136,50 +136,5 @@ class Flowers implements Flower{
         return actualBought;
 
     }
-    JFrame editorFrame;
 
-    ImageIcon imageIcon;
-
-    BufferedImage image;
-
-    JLabel jLabel;
-
-    public void view() {
-
-        editorFrame = new JFrame("MintFlower");
-
-        editorFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-
-        image=null;
-
-        try{
-
-            image = ImageIO.read(new File("D:/mintflower.jpg"));
-
-        }
-
-        catch (Exception e)
-
-        {
-
-            e.printStackTrace();
-
-            System.exit(1);
-
-        }
-
-        imageIcon = new ImageIcon(image);
-
-        jLabel = new JLabel();
-
-        jLabel.setIcon(imageIcon);
-
-        editorFrame.getContentPane().add(jLabel, BorderLayout.CENTER);
-
-        editorFrame.pack();
-
-        editorFrame.setLocationRelativeTo(null);
-
-        editorFrame.setVisible(true);
-    }
 }
